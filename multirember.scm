66dffc1d3d0d1cc1d6afe8b880c9af4c331c2971
bintabaldeh <mamadoubaldeh921@gmail.com>
2019-02-18 12:45:06 +0900

    multirember function

---
+++ multirember.scm
@@ -0,0 +1,13 @@
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+        (cond
+          ((eq? (car lat) a)
+            (multirember a(cdr lat)))
+            (else (cons (car lat)
+            (multirember a(cdr lat)))))))))
+(print (multirember 'apple '(apple banana orange)))
+(print (multirember 'apple '(apple banana orange apple)))
+(print (multirember 'hoge '(hoge hoge hoge)))
