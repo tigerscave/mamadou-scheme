1c98c1b5ce32f6a25bdf740394f4146c55d49e5d
bintabaldeh <mamadoubaldeh921@gmail.com>
2019-03-11 22:48:15 +0900

    0306 function

--- 0306.scm
+++ 0306.scm
@@ -31,11 +31,36 @@
 
 
 ;Q3. write multirember
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+        (else
+          (cond
+            ((eq? (car lat) a)
+              (multirember a (cdr lat)))
+            (else (cons (car lat)
+                  (multirember a (cdr lat)))))))))
+(print (multirember 'a '(a b c a)))
+
 
 ;(print (rember 'a '(a b c a)))
 
 
 
 ;Q4. write reber*
+(define rember*
+  (lambda (a l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+        (cond
+          ((eq? (car l) a)
+            (rember* a (cdr l)))
+            (else (cons (car l)
+              (rember* a(cdr l))))))
+              (else (cons (rember* a(car l))
+              (rember* a (cdr l)))))))
+(print (rember* 'a '(a b c (a) a)))
 
 ;(print (rember* 'a '(a b c (a) a)))
