8238cad04165ddacda60703ac0ee807957082aec
bintabaldeh <mamadoubaldeh921@gmail.com>
2019-03-17 01:45:08 +0900

    0316 function

---
+++ 0316.scm
@@ -0,0 +1,44 @@
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+        ((atom? (car l)) (lat? (cdr l)))
+          (else #f))))
+(define atom?
+  (lambda (x)
+  (and (not (pair? x)) (not (null? x)))))
+;(print (atom? '()))
+;#f
+;(print (atom '(a b c)))
+;#t
+(print (lat?  '(a b c)))
+(print (lat? '()))
+(print (lat? '(())))
+(print (lat? '((a b c))))
+;#t
+;#t
+;(print (rember 'apple '(apple banana orange)))
+;(banana orange)
+;(print (rember 'apple '(apple banana apple orange)))
+;(banana apple orange)
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) a) (cdr lat))
+      (else (rember a (cdr lat))))))
+
+(print (rember 'apple '(apple banana )))
+(print (rember 'apple '(apple apple apple)))
+
+;(print (sub1 6))
+; => 5
+;(print (sub1 3))
+; => 2
+(define sub1
+  (lambda (x)
+    (- x 1)))
+
+(print (sub1 6))
+(print (sub1 3))
