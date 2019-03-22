41df30b29134f8fd720d8e3a904da1def102130a
bintabaldeh <mamadoubaldeh921@gmail.com>
2019-03-22 12:23:47 +0900

    0322 fucntion

---
+++ 0322.scm
@@ -0,0 +1,13 @@
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+            ((eq? (car lat) old)
+              (cons new
+                (cons old (cdr lat))))
+            (else (cons (car lat)
+                  (insertL new old (cdr lat)))))))))
+
+;(ice cream with topping fudge for dessert)
+(print (insertL 'topping 'fudge '(ice cream with fudge for dessert)))
