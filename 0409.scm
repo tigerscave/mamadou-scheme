c6729ee0f7d600bfaf354c02664bd0ba388907be
bintabaldeh <mamadoubaldeh921@gmail.com>
2019-04-11 12:38:01 +0900

    0409 function

--- 0409.scm
+++ 0409.scm
@@ -6,10 +6,9 @@
       (else (cons (car lat)
               (rm-even* (cdr lat)))))))
 
-;(print (rm-even* '(2 4 6 8 10)))
-;(print (rm-even* '(1 2 3 4 5)))
+(print (rm-even* '(2 4 6 8 10)))
+(print (rm-even* '(1 2 3 4 5)))
 
 (print (rm-even* '(1 2 3 4)))
-(print (rm-even* '(2 4 6 8)))
 (print (rm-even* '(1 2 3 4)))
 (print (rm-even* '(2 4 6 8)))
