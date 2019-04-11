15ebdcf5ef706e11eb8f69c0e58d79b37fb2577d
bintabaldeh <mamadoubaldeh921@gmail.com>
2019-04-11 11:51:38 +0900

    0409 function

--- 0409.scm
+++ 0409.scm
@@ -1,11 +1,10 @@
-(define rm-even-star
+(define rm-even*
   (lambda (lat)
     (cond
       ((null? lat) (quote ()))
-        (else (cond
-          ((even? (car lat)) (rm-even-star (cdr lat)))
-            (else (cons (car lat)
-              (rm-even-star (cdr lat)))))))))
+      ((even? (car lat)) (rm-even* (cdr lat)))
+      (else (cons (car lat)
+              (rm-even* (cdr lat)))))))
 
-(print (rm-even-star '(2 4 6 8 10)))
-(print (rm-even-star '(1 2 3 4 5)))
+(print (rm-even* '(2 4 6 8 10)))
+(print (rm-even* '(1 2 3 4 5)))
