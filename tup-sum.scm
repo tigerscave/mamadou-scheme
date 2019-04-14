99d652f70ff4c374215f628347ea5268749f1ad8
bintabaldeh <mamadoubaldeh921@gmail.com>
2019-04-14 23:05:48 +0900

    tup-sum.scm

--- tup-sum.scm
+++ tup-sum.scm
@@ -7,3 +7,22 @@
 
 (print (tup-sum '(2 4 6 8)))
 (print (tup-sum '(1 2 3 4)))
+
+
+(define rember*
+  (lambda (a l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+        (cond
+          ((eq? (car l) a)
+            (rember* a (cdr l)))
+            (else (cons (car l)
+              (rember* a(cdr l))))))
+              (else (cons (rember* a(car l))
+              (rember* a (cdr l)))))))
+
+(define atom?
+  (lambda (x)
+  (and (not (null? x)) (not (pair? x)))))
+(print (rember* 'sauce '(((tomato sauce))((bean) sauce)(and ((flying)) sauce))))
