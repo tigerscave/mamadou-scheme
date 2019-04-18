94faa3c44a62df5d2360279b39c02277d0bf90c9
bintabaldeh <mamadoubaldeh921@gmail.com>
2019-04-18 22:46:33 +0900

    fix indent an spaces

--- 0411.scm
+++ 0411.scm
@@ -6,9 +6,9 @@
         (cond
           ((eq? (car l) a)
             (rember* a (cdr l)))
-            (else (cons (car l)
-              (rember* a(cdr l))))))
-              (else (cons (rember* a(car l))
+          (else (cons (car l)
+                  (rember* a (cdr l))))))
+      (else (cons (rember* a (car l))
               (rember* a (cdr l)))))))
 (define atom?
   (lambda (x)
@@ -28,7 +28,6 @@
 
 (print (rm-even* '(2 4 6 8 10)))
 (print (rm-even* '(1 2 3 4 5)))
-
 (print (rm-even* '(1 2 3 4)))
 (print (rm-even* '(1 2 3 4)))
 (print (rm-even* '(2 4 6 8)))
@@ -37,7 +36,7 @@
   (lambda (tup)
     (cond
       ((null? tup) 0)
-      (else (+ (car tup) (sum-tup*( cdr tup)))))))
+      (else (+ (car tup) (sum-tup* (cdr tup)))))))
 
 (print (sum-tup* '(1 2 3 4)))
 (print (sum-tup* '(12 32 48 6)))
@@ -45,8 +44,8 @@
 (define tup-sum
   (lambda (tup)
     (cond
-    ((null? tup) 0)
-    (else (+ (car tup) (tup-sum (cdr tup)))))))
-(print (tup-sum '(1 2 3 4)))
+      ((null? tup) 0)
+      (else (+ (car tup) (tup-sum (cdr tup)))))))
 
+(print (tup-sum '(1 2 3 4)))
 (print (tup-sum '(2 4 6 8)))
