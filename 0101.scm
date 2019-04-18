a009c523492b5c1ab88296d45188e36aa3a1c0bb
bintabaldeh <mamadoubaldeh921@gmail.com>
2019-04-18 22:15:00 +0900

    fix style what pointed in the review

--- 0101.scm
+++ 0101.scm
@@ -3,16 +3,14 @@
     (cond
       ((null? tup) 0)
       ((number? tup) tup)
-      (else (+ (sum-tup-in-tup (car tup)) (sum-tup-in-tup (cdr tup))))
-    )
-  )
-)
+      (else (+ (sum-tup-in-tup (car tup)) (sum-tup-in-tup (cdr tup)))))))
 
 (print (sum-tup-in-tup '(2 3 (4 5) 5 7)))
 
 (define function_sq-xy-sub
   (lambda (x y)
     (* (- x y) (- x y))))
+
 (print (function_sq-xy-sub 10 5))
 (print (function_sq-xy-sub 4 5))
 (print (function_sq-xy-sub 7 3))
@@ -21,10 +19,7 @@
   (lambda (n m)
     (cond
       ((zero? m) 1)
-      (else (* n (power n (- m 1))))
-    )
-  )
-)
+      (else (* n (power n (- m 1)))))))
 
 (print (power 5 3))
 
@@ -33,9 +28,6 @@
     (cond
       ((null? tup) 0)
       ((number? tup) tup)
-      (else (+ (car tup) (tup-sum (cdr tup))))
-    )
-  )
-)
+      (else (+ (car tup) (tup-sum (cdr tup)))))))
 
 (print (sum-tup-in-tup '(2 3 4 5 6)))
